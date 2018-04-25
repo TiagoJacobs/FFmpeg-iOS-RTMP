c28c39067d3dd978ad9800545baa35539c75c242
Joao Siebel <joaos_desenv@imdt.com.br>
2018-04-25 16:49:40 -0300

    format tar

--- build-libssl.sh
+++ build-libssl.sh
@@ -14,6 +14,6 @@ echo "-----"
 echo "SSL framework created at `cd openssl.framework/; pwd`"
 echo "-----"
 
-tar -cvzf /libssl.tar.gz `cd openssl.framework/; pwd`
+tar cvzf /libssl.tar.gz `cd openssl.framework/; pwd`
 
 cd -
