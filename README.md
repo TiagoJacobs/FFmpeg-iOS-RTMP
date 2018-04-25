00f65381e3e6edc01c649b8c4756224105182555
Tiago Daniel Jacobs <tiago@imdt.com.br>
2018-04-25 12:02:56 -0300

    Add SSL script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # FFmpeg-iOS-RTMP
 FFmpeg iOS RTMP build script
+
+
+# Steps
+
+**Build the OpenSSL library**
+
+```
+git clone git@github.com:tdjac0bs/FFmpeg-iOS-RTMP.git
+cd FFmpeg-iOS-RTMP/
+./build-libssl.sh
+```
---
+++ build-libssl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+rm -rf work/ssl/
+mkdir -p work/ssl/
+cd work/ssl/
+
+git clone https://github.com/tdjac0bs/OpenSSL-for-iPhone.git
+cd OpenSSL-for-iPhone/
+ ./build-libssl.sh --version=1.0.2o
+./create-openssl-framework.sh
+
+echo "-----"
+echo "SSL framework created at `cd openssl.framework/; pwd`"
+echo "-----"
+
+cd -
